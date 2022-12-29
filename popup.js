0e6781b3116477089f869ec56b4130c9bf338fdd
phihash <insharp0220@gmail.com>
2022-12-30 03:26:38 +0900

    グループ化するボタンもセットで実装し、localstorageでデータをtabid=>グループ名とグループ名=>タブIDの配列を保存するようにした

--- popup.js
+++ popup.js
@@ -1,30 +1,60 @@
 const tabList = document.getElementById("tabList");
+const groupInput = document.getElementById("groupInput");
 
-chrome.tabs.onRemoved.addListener(async () => {
+const onGroup = async (e) => {
+  let message = groupInput.value;
+  let groupTabs = JSON.parse(localStorage.getItem(message));
+  localStorage.setItem(e.currentTarget.id,message);
+  if(!groupTabs){
+   localStorage.setItem(message,JSON.stringify([e.currentTarget.id]));
+  }else{
+    localStorage.setItem(message,JSON.stringify([...groupTabs,e.currentTarget.id]));
+  }
+  groupInput.value = "";
+  // viewTabs(getTabs);
   let getTabs = await chrome.tabs.query({currentWindow: true});
   viewTabs(getTabs);
-});
 
+}
 
 const onClose = (e) => {
   console.log(e.currentTarget.id);
   let removing = chrome.tabs.remove(Number(e.currentTarget.id));
   console.log(removing);
+  //TODO 閉じるボタン押されたらonGroup解除しないといけない
+
 }
 
-
-
-const addTab = (tab,index) => {
-  const h3Element = document.createElement("h3");
+const addTab =  (tab,index) => {
+  const tabListItemElement = document.createElement("div");
+  const h3Element = document.createElement("p");
   const removeButton = document.createElement("button");
+  const groupButton = document.createElement("button");
   h3Element.innerText = `${index + 1} ${tab.title} `;
   removeButton.id =  tab.id;
-  removeButton.textContent = "閉じる";
+  groupButton.id =  tab.id;
+  let getGroup =  localStorage.getItem(tab.id);
+  removeButton.textContent = "x";
+  groupButton.textContent = "グループ";
   removeButton.addEventListener("click",onClose);
+  groupButton.addEventListener("click",onGroup);
   tabList.appendChild(h3Element);
-  tabList.appendChild(removeButton);
+  if(getGroup){
+    const spanElement = document.createElement("span");
+      spanElement.innerHTML = getGroup;
+      tabList.appendChild(spanElement)
+  }
+  tabListItemElement.appendChild(h3Element);
+  tabListItemElement.appendChild(removeButton);
+  tabListItemElement.appendChild(groupButton);
+  tabListItemElement.className = "d-flex"
+  tabList.appendChild(tabListItemElement);
 }
 
+chrome.tabs.onRemoved.addListener(async () => {
+  let getTabs = await chrome.tabs.query({currentWindow: true});
+  viewTabs(getTabs);
+});
 
 const viewTabs = (getTabs) => {
   tabList.innerHTML = "";
