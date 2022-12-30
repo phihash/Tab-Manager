2d8d1e2c4402fd1ead2c620ffeff2e11e2fd170e
phihash <insharp0220@gmail.com>
2022-12-31 00:50:05 +0900

    groupボタン削除した

--- popup.js
+++ popup.js
@@ -1,52 +1,24 @@
 const tabList = document.getElementById("tabList");
-const groupInput = document.getElementById("groupInput");
-
-const onGroup = async (e) => {
-  let message = groupInput.value;
-  let groupTabs = JSON.parse(localStorage.getItem(message));
-  localStorage.setItem(e.currentTarget.id,message);
-  if(!groupTabs){
-   localStorage.setItem(message,JSON.stringify([e.currentTarget.id]));
-  }else{
-    localStorage.setItem(message,JSON.stringify([...groupTabs,e.currentTarget.id]));
-  }
-  groupInput.value = "";
-  // viewTabs(getTabs);
-  let getTabs = await chrome.tabs.query({currentWindow: true});
-  viewTabs(getTabs);
-
-}
 
 const onClose = (e) => {
-  console.log(e.currentTarget.id);
-  let removing = chrome.tabs.remove(Number(e.currentTarget.id));
+  console.log(e.target.id);
+  let removing = chrome.tabs.remove(Number(e.target.id));
   console.log(removing);
   //TODO 閉じるボタン押されたらonGroup解除しないといけない
-
+  localStorage.removeItem(e.target.id);
 }
 
 const addTab =  (tab,index) => {
   const tabListItemElement = document.createElement("div");
   const h3Element = document.createElement("p");
-  const removeButton = document.createElement("button");
-  const groupButton = document.createElement("button");
+  const tabCheckbox = document.createElement("input");
+  tabCheckbox.setAttribute("type","checkbox")
   h3Element.innerText = `${index + 1} ${tab.title} `;
-  removeButton.id =  tab.id;
-  groupButton.id =  tab.id;
-  let getGroup =  localStorage.getItem(tab.id);
-  removeButton.textContent = "x";
-  groupButton.textContent = "グループ";
-  removeButton.addEventListener("click",onClose);
-  groupButton.addEventListener("click",onGroup);
+  tabCheckbox.id =  tab.id;
+  tabCheckbox.addEventListener("click",onClose);
   tabList.appendChild(h3Element);
-  if(getGroup){
-    const spanElement = document.createElement("span");
-      spanElement.innerHTML = getGroup;
-      tabList.appendChild(spanElement)
-  }
   tabListItemElement.appendChild(h3Element);
-  tabListItemElement.appendChild(removeButton);
-  tabListItemElement.appendChild(groupButton);
+  tabListItemElement.appendChild(tabCheckbox);
   tabListItemElement.className = "d-flex"
   tabList.appendChild(tabListItemElement);
 }
@@ -64,6 +36,10 @@ const viewTabs = (getTabs) => {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  let getTabs = await chrome.tabs.query({currentWindow: true});
-  viewTabs(getTabs);
+  let currentTabs = await chrome.tabs.query({currentWindow: true});
+  viewTabs(currentTabs);
 });
+
+chrome.tabs.onUpdated.addListener(() => {
+  window.alert("復活したな！")
+})
